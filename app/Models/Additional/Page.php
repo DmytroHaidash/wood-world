153df25b25c5899dd2ee56d6677de3ff08bdb3e2
Dmytro Haidash <dmyto.haidash@gmail.com>
2020-09-19 22:49:12 +0300

    meta

--- app/Models/Additional/Page.php
+++ app/Models/Additional/Page.php
@@ -2,7 +2,9 @@
 
 namespace App\Models\Additional;
 
+use App\Models\Service\Meta;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Spatie\MediaLibrary\HasMedia\HasMedia;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 use Talanoff\ImpressionAdmin\Traits\Translatable;
