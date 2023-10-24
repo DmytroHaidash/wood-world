ed31fd9712958b2ebbe50ea4d3b94e77b7a19859
Dmytro Haidash <dmyto.haidash@gmail.com>
2023-10-23 21:34:58 -0500

    translates

--- resources/lang/en/forms.php
+++ resources/lang/en/forms.php
@@ -1,12 +1,14 @@
 <?php
 
 return [
-	'name' => 'Your name',
-	'contact' => 'У-mail',
-	'message' => 'Question?',
-
-	'buttons' => [
-		'buy' => 'Buy',
-		'question' => 'Ask a question',
-	],
+    'name' => 'Your name',
+    'contact' => 'E-mail',
+    'buttons' => [
+        'buy' => 'Buy',
+        'question' => 'Ask a question',
+    ],
+    'message' => [
+        'order' => 'Message <small class="text-muted"> (not required) </small>',
+        'question' => 'Your question:',
+    ]
 ];
