37fd7a472e19ed67369ce332fe1abb87aa791c92
Dmytro Haidash <dmyto.haidash@gmail.com>
2023-11-12 21:23:59 -0600

    changes

--- resources/views/partials/app/layout/header.blade.php
+++ resources/views/partials/app/layout/header.blade.php
@@ -1,5 +1,5 @@
 <header id="app-header">
-    <div class="container-fluid">
+    <div class="container-fluid--header">
 
         <div class="row align-items-center">
             <div class="col-auto">
